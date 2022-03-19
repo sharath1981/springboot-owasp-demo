7031dedb3c4336ff18f3b2e2c3b37962ee92ee9e
SHARATH KUMAR B <sharath1981@gmail.com>
2022-03-19 09:24:20 +0530

    first commit

---
+++ src/main/java/com/ryana/SpringbootOwaspDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ryana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOwaspDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOwaspDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryana/SpringbootOwaspDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOwaspDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
